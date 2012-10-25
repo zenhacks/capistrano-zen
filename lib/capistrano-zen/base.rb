2ea6b7f2e3d5b9d012de0d6b44a7179a38401a28
Steven Yang <yangchenyun@gmail.com>
2012-10-26 01:23:09 +0800

    corrected the load path

--- lib/capistrano-zen/base.rb
+++ lib/capistrano-zen/base.rb
@@ -1,14 +1,11 @@
+require 'capistrano-zen/common'
+
 # Bootstrap Capistrano instance
 configuration = Capistrano::Configuration.respond_to?(:instance) ?
   Capistrano::Configuration.instance(:must_exist) :
   Capistrano.configuration(:must_exist)
 
 configuration.load do
-  def template(from, to)
-    erb = File.read(File.expand_path("../tmpls/#{from}", __FILE__))
-    put ERB.new(erb).result(binding), to
-  end
-
   namespace :deploy do
     desc "Install everything onto the server"
     task :install do
