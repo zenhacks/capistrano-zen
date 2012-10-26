f991a5e0e4251026dc4429d88a9c274b14be6b2c
Steven Yang <yangchenyun@gmail.com>
2012-10-26 12:58:38 +0800

    move dev_lib tasks into utils

--- lib/capistrano-zen/utils.rb
+++ lib/capistrano-zen/utils.rb
@@ -11,12 +11,22 @@ configuration.load do
 
   namespace :check do
     desc "Make sure local git is in sync with remote."
-    task :revision, roles: :web do
+    task :revision do
       unless `git rev-parse #{branch}` == `git rev-parse origin/#{branch}`
         puts "WARNING: HEAD is not the same as origin/#{branch}"
         puts "Run `git push` to sync changes."
         exit
       end
+    end
+  end
+
+  namespace :dev_lib do
+    task :install do
+      # nokogiri dependencies
+      run "#{sudo} apt-get install libxslt-dev libxml2-dev"
+
+      # paperclip dependencies
+      run "#{sudo} apt-get install imagemagick"
     end
   end
 end
