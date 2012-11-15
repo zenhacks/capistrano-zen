3b00a0cdc414d3869c3ba295bcab8244c1a45aff
Steven Yang <yangchenyun@gmail.com>
2012-11-15 16:38:00 +0800

    Shortened postgresql variables

--- lib/capistrano-zen/postgresql.rb
+++ lib/capistrano-zen/postgresql.rb
@@ -13,31 +13,31 @@ configuration.load do
   DB_FILE_PATH = "#{pg_config_path}/database.yml"
   DBCONFIG = YAML.load_file(DB_FILE_PATH)
 
-  _cset(:postgresql_host) { DBCONFIG['production']['host'] }
-  _cset(:postgresql_user) { DBCONFIG['production']['username'] }
-  _cset(:postgresql_password) { DBCONFIG['production']['password'] }
-  _cset(:postgresql_database) { DBCONFIG['production']['database'] }
+  _cset(:psql_host) { DBCONFIG['production']['host'] }
+  _cset(:psql_user) { DBCONFIG['production']['username'] }
+  _cset(:psql_password) { DBCONFIG['production']['password'] }
+  _cset(:psql_database) { DBCONFIG['production']['database'] }
 
   namespace :pg do
-    desc "Install the latest stable release of PostgreSQL."
+    desc "Install the latest stable release of psql."
     task :install, roles: :db, only: {primary: true} do
-      run "#{sudo} add-apt-repository -y ppa:pitti/postgresql"
+      run "#{sudo} add-apt-repository -y ppa:pitti/psql"
       run "#{sudo} apt-get -y update"
-      run "#{sudo} apt-get -y install postgresql libpq-dev"
+      run "#{sudo} apt-get -y install psql libpq-dev"
     end
 
     desc "Create a database for this application."
     task :init, roles: :db, only: { primary: true } do
       # reset the database and role
-      run %Q{#{sudo} -u postgres psql -c "CREATE USER #{postgresql_user} WITH PASSWORD '#{postgresql_password}';"}
-      run %Q{#{sudo} -u postgres psql -c "CREATE DATABASE #{postgresql_database} OWNER #{postgresql_user};"}
+      run %Q{#{sudo} -u postgres psql -c "CREATE USER #{psql_user} WITH PASSWORD '#{psql_password}';"}
+      run %Q{#{sudo} -u postgres psql -c "CREATE DATABASE #{psql_database} OWNER #{psql_user};"}
     end
 
     desc "Reset the database and role for this application."
     task :reset, roles: :db, only: { primary: true } do
       # drop the database and role
-      run %Q{#{sudo} -u postgres psql -c "DROP DATABASE #{postgresql_database};"}
-      run %Q{#{sudo} -u postgres psql -c "DROP ROLE #{postgresql_user};"}
+      run %Q{#{sudo} -u postgres psql -c "DROP DATABASE #{psql_database};"}
+      run %Q{#{sudo} -u postgres psql -c "DROP ROLE #{psql_user};"}
     end
 
     desc "Generate the database.yml configuration file."
@@ -54,9 +54,9 @@ configuration.load do
     desc "Dump the application's database to backup path."
     task :dump, roles: :db, only: { primary: true } do
       # ignore migrations / exclude ownership / clean restore
-      run "pg_dump #{postgresql_database} -T '*migrations' -O -c -U #{postgresql_user} -h localhost | gzip > #{pg_backup_path}/#{application}-#{release_name}.sql.gz" do |channel, stream, data|
+      run "pg_dump #{psql_database} -T '*migrations' -O -c -U #{psql_user} -h localhost | gzip > #{pg_backup_path}/#{application}-#{release_name}.sql.gz" do |channel, stream, data|
         puts data if data.length >= 3
-        channel.send_data("#{postgresql_password}\n") if data.include? 'Password'
+        channel.send_data("#{psql_password}\n") if data.include? 'Password'
       end
     end
   end
