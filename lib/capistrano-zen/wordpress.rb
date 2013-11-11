e25f83df542258ca7408ff3878d4de1968f01db4
Steven Yang <yangchenyun@gmail.com>
2013-11-11 18:50:42 +0800

    added cap templates for wordpress deployment

---
+++ lib/capistrano-zen/wordpress.rb
@@ -0,0 +1,19 @@
+require 'capistrano-zen/base'
+
+configuration = Capistrano::Configuration.respond_to?(:instance) ?
+  Capistrano::Configuration.instance(:must_exist) :
+  Capistrano.configuration(:must_exist)
+
+configuration.load do
+  namespace :wordpress do
+    desc "upload the configuration to remote server"
+    task :config, roles: :web do
+      template "wp-config.php.erb", "/tmp/wp-config.php"
+      run "#{sudo} mv /tmp/wp-config.php #{deploy-to}/"
+    end
+  end
+end
+
+def random_salts
+  ('.'..'z').to_a.shuffle[0,60].join
+end
